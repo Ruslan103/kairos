3a9332733c61d881ca21923924f1380e9a3b163e
zaharovra <orrriole@yndex.ru>
2026-09-19 14:30:52 +0400

    Bump version to 1.1.2 for release APK.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.kaizenkanban"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
